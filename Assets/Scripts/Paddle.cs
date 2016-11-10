96556b13a3bb3900d51bea5b1838fa9ea890f6f8
supergajic <supergajic@gmail.com>
2016-11-11 00:47:33 +0100

    Added Google VR plugin for Unity

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,18 +5,32 @@ public class Paddle : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        Input.gyro.enabled = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 paddlePos = new Vector3(this.transform.position.x, this.transform.position.y, 10f); // x = -2.3 till 2.3  y = -0.4  z = Alltid 10
+        //Vector3 paddlePos = new Vector3(this.transform.position.x, this.transform.position.y, 10f); // x = -2.3 till 2.3  y = -0.4  z = Alltid 10
+        Vector3 paddlePos = new Vector3(Input.acceleration.x, this.transform.position.y , 10f); // x = -2.3 till 2.3  y = -0.4  z = Alltid 10
 
+        //float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
         float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
 
         paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+        //paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
 
         this.transform.position = paddlePos;
 	}
+
+    //void Start()
+    //{
+    //    Input.gyro.enabled = true;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+    //}
 }
