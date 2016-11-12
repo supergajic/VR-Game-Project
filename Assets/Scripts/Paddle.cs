a3f6683f7f4cd433353f366f4efbbfdfd964f1bd
supergajic <supergajic@gmail.com>
2016-11-12 01:12:17 +0100

    Controller test

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -11,14 +11,14 @@ public class Paddle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        //Vector3 paddlePos = new Vector3(this.transform.position.x, this.transform.position.y, 10f); // x = -2.3 till 2.3  y = -0.4  z = Alltid 10
-        Vector3 paddlePos = new Vector3(Input.acceleration.x, this.transform.position.y , 10f); // x = -2.3 till 2.3  y = -0.4  z = Alltid 10
+        Vector3 paddlePos = new Vector3(this.transform.position.x, this.transform.position.y, 10f); // x = -2.3 till 2.3  y = -0.4  z = Alltid 10
+        //Vector3 paddlePos = new Vector3(Input.acceleration.x, this.transform.position.y , 10f); // x = -2.3 till 2.3  y = -0.4  z = Alltid 10 FUNKAR!
 
-        //float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        //float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16; // FUNKAR!
+        float mousePosInBlocks = Input.GetAxis("Horizontal") / Screen.width * 16;
 
         paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
-        //paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+        //paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f); //FUNKAR!
 
         this.transform.position = paddlePos;
 	}
