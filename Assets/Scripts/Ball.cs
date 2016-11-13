5bdb4abdb8fe5b49431a0e83ed7bd7c96f08b19d
supergajic <supergajic@gmail.com>
2016-11-13 02:40:14 +0100

    Updated Brick sizes & Ball speed

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,10 +6,13 @@ public class Ball : MonoBehaviour {
     public Paddle paddle;
     private bool hasStarted = false;
     private Vector3 paddleToBallVector;
+    private float countdown;
+    private float constantSpeed = 8f;
 
 	// Use this for initialization
 	void Start () {
         paddleToBallVector = this.transform.position - paddle.transform.position;
+        countdown = 5f;
 	}
 	
 	// Update is called once per frame
@@ -18,13 +21,22 @@ public class Ball : MonoBehaviour {
         if (!hasStarted)
         {
             this.transform.position = paddle.transform.position + paddleToBallVector;
+            countdown -= Time.deltaTime;
         }
 
-        if(Input.GetMouseButtonDown(0))
+        if(countdown < 0)
         {
+            countdown = 5f;
             hasStarted = true;
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 6f);
+            this.GetComponent<Rigidbody2D>().velocity = constantSpeed * (this.GetComponent<Rigidbody2D>().velocity.normalized);
             //this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f); // 10f är för hastigheten.
         }
 	}
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+
+    //    this.GetComponent<Rigidbody2D>().velocity += tweak;
+    //}
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,7 +5,6 @@ public class Paddle : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Input.gyro.enabled = true;
     }
 	
 	// Update is called once per frame
@@ -15,7 +14,7 @@ public class Paddle : MonoBehaviour {
         //Vector3 paddlePos = new Vector3(Input.acceleration.x, this.transform.position.y , 10f); // x = -2.3 till 2.3  y = -0.4  z = Alltid 10 FUNKAR!
 
         //float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16; // FUNKAR!
-        float mousePosInBlocks = Input.GetAxis("Horizontal") / Screen.width * 16;
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
 
         paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
         //paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f); //FUNKAR!
