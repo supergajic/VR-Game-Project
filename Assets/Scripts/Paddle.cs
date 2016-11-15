50ac05098ae0e6e8e55141214ea468098013a9d3
supergajic <supergajic@gmail.com>
2016-11-15 18:36:36 +0100

    Added Beta scene

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
-    public GameObject paddle;
+    //public GameObject paddle;
     private int speed;
 
 	// Use this for initialization
@@ -23,16 +23,16 @@ public class Paddle : MonoBehaviour {
 
         //if (this.transform.position.x < -5 && this.transform.position.x < 30)
         //{
-        if (paddle.transform.position.x > -5 && paddle.transform.position.x < 28)
-        {
-            transform.Translate(Vector3.right * Input.GetAxis("RightJoystickX") * speed * Time.deltaTime);
-        }
-        else
-        {
-            speed = 0;
-        }
+        //if (paddle.transform.position.x > -5 && paddle.transform.position.x < 28)
+        //{
+           transform.Translate(Vector3.right * Input.GetAxis("RightJoystickX") * speed * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    speed = 0;
+        //}
 
-        //paddlePos.x = Mathf.Clamp(mousePosInBlocks, -8f, 28f);
+        //paddle = Mathf.Clamp(mousePosInBlocks, -8f, 28f);
         //paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f); //FUNKAR!
 
         //this.transform.position = paddlePos;
